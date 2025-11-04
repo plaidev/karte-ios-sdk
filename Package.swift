7fb993a9e52ca7d93f3b14f009c8104c3a01228b
snishimura <sojiro.nishimura@plaid.co.jp>
2025-11-04 14:13:05 +0900

    Fix/fix build issue for crashreporting (#671)
    * update iOS version
    
    * update versions
    
    * update CHANGELOG
    
    ---------
    
    Co-authored-by: Sojiro Nishimura <sojiro.nishimura@macbookpro.lan>

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Karte",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
